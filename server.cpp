b8c63c038c06ff2ca1352a86786560c3fe1d5734
Gustavo Lugo <gustavolugo1010@gmail.com>
2026-08-31 20:12:06 -0700

    created client variable, assigned port, assigned buffer size, created client struct and added function for sending a message to one client to server.cpp

--- server.cpp
+++ server.cpp
@@ -13,3 +13,27 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+using namespace std;
+
+const int PORT = 54000;
+const int BUFFER_SIZE = 1024;
+
+struct Client 
+{
+	int socket;
+	string username;
+};
+
+vector<Client> clients;
+mutex clientsMutex;
+
+//sending a message to a client
+void sendMessage(int clientSocket, const string& message)
+{
+	send(
+		clientSocket,
+		message.c_str(),
+		message.size(),
+		0
+	);
+	
