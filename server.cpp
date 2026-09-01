b4fe25ea6391b98249fd4c392d36b3858144defa
Gustavo Lugo <gustavolugo1010@gmail.com>
2026-08-31 19:57:07 -0700

    added the libraries to server.cpp

--- server.cpp
+++ server.cpp
@@ -1,1 +1,15 @@
+ /*
+ * Author: Gustavo Lugo
+ * 08-25-2026
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <algorithm>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
