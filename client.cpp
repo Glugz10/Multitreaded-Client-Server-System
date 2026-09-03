c747d5544ef269f2a4d490d1e43a29fd1920b160
Gustavo Lugo <gustavolugo1010@gmail.com>
2026-09-02 20:27:36 -0700

    Initializing variables and libraries

--- client.cpp
+++ client.cpp
@@ -3,3 +3,17 @@
  * 08-25-2026
  */
 
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+using namespace std:
+
+const string SERVER_IP = "127.0.0.1";
+const int PORT = 54000;
+const int BUFFER_SIZE = 1024;
+
