16d9924f2f5522f69454275b934160c91cdf65ae
Gustavo Lugo <gustavolugo1010@gmail.com>
2026-09-02 20:03:21 -0700

    Debugging

--- server.cpp
+++ server.cpp
@@ -1,4 +1,4 @@
- /*
+/*
  * Author: Gustavo Lugo
  * 08-25-2026
  */
@@ -18,268 +18,357 @@ using namespace std;
 const int PORT = 54000;
 const int BUFFER_SIZE = 1024;
 
-struct Client 
+struct Client
 {
-	int socket;
-	string username;
+    int socket;
+    string username;
 };
 
 vector<Client> clients;
 mutex clientsMutex;
 
-//sending a message to a client
+
+// Sending a message to a client
 void sendMessage(int clientSocket, const string& message)
 {
-	send(
-		clientSocket,
-		message.c_str(),
-		message.size(),
-		0
-	);
-	
-//sending a message to every connected client
+    send(
+        clientSocket,
+        message.c_str(),
+        message.size(),
+        0
+    );
+}
+
+
+// Sending a message to every connected client
 void broadcastMessage(const string& message, int senderSocket = -1)
 {
-	vector<int> clientSockets;
-	
-	{
-		lock_guard<mutex> lock(clientsMutex);
-		for (const Client& client : clients)
-		{
-			if(client.socket != senderSocket)
-			{
-				clientSockets.push_back(client.socket);
-			}
-		}
-	}
-	
-	for(int socket : clientSockets)
-	{
-		sendMessage(socet, message);
-	}
+    vector<int> clientSockets;
+
+    {
+        lock_guard<mutex> lock(clientsMutex);
+
+        for (const Client& client : clients)
+        {
+            if (client.socket != senderSocket)
+            {
+                clientSockets.push_back(client.socket);
+            }
+        }
+    }
+
+    for (int socket : clientSockets)
+    {
+        sendMessage(socket, message);
+    }
 }
 
-//Removing a client from the connected list
+
+// Removing a client from the connected list
 void removeClient(int clientSocket)
 {
-	lock_guard<mutex> lock(clientsMutex);
-	
-	clients.erase(
-		remove_if(
-			clients.begin(),
-			clients.end(),
-			[clientSocket](const Client& client)
-			{
-				return client.socket == clientSocket;
-			});
-		clients.end()
-		);
+    lock_guard<mutex> lock(clientsMutex);
+
+    clients.erase(
+        remove_if(
+            clients.begin(),
+            clients.end(),
+            [clientSocket](const Client& client)
+            {
+                return client.socket == clientSocket;
+            }
+        ),
+        clients.end()
+    );
 }
 
-//Return a list of connected users
+
+// Return a list of connected users
 string getUserList()
 {
-	lock_guard<mutex> lock(clientsMutex);
-	
-	string result = "\nConnected users:\n";
-	
-	for(const Client& client : clients)
-	{
-		result += "- " + client.username + "\n";
-	}
-	return result;
+    lock_guard<mutex> lock(clientsMutex);
+
+    string result = "\nConnected users:\n";
+
+    for (const Client& client : clients)
+    {
+        result += "- " + client.username + "\n";
+    }
+
+    return result;
 }
 
-//Handling communication with a client
+
+// Handling communication with a client
 void handleClient(int clientSocket)
 {
-	char buffer[BUFFER_SIZE];
-	
-	//prompt client for username
-	string request = "Enter your username: ";
-	sendMessage(clientSocket, request);
-	int bytesReceived = recv(
-		clientSocket, 
-		buffer,
-		BUFFER_SIZE,
-		0
-	);
-	
-	if(bytesReceived <= 0)
-	{
-		close(clientSockets);
-		return;	
-	}
-	
-	string username(buffer, bytesReceived);
-	
-	//Remove trailing newLine characters
-	while(!username.empty() && (username.back() == '\n' || username.back() == '\r'))
-	{
-		username.pop_back();
-	}
-	
-	//Add client to shared client list
-	{
-		lock_guard<mutex> lock(clientsMutex);
-		
-		Client newClient;
-		
-		newClient.socket = clientSocket;
-		newClient.username = username;
-		
-		clients.push_back(newClients);
-	}
-	
-	cout << username << " connected." << endl;
-	
-	string joinMessage = "\n[SERVER]" + username + " joined the chat. \n";
-	
-	broadcastingMessage(joinMessage, clientSocket);
-	
-	
-	string welcomeMessage = "\nWelcome, " + username + "!\n" 
-		"Commands:\n" 
-		"/users - show connected users\n"
-		"/quit - disconnected\n\n";
-		
-	sendMessage(clientSocket, welcomeMessage);
-	
-	//Receive messages from this client
-	while(true)
-	{
-		bytesReceived = recv(
-			clientSocket, 
-			buffer, 
-			BUFFER_SIZE, 
-			0
-		);
-		
-		if(bytesReceived <= 0)
-		{
-			break;
-		}
-		
-		string message( buffer, bytesReceived);
-		 
-	//Removing trail newLine Characters
-	while(!message.empty() && (message.back() =='\n' || message.back() == '\r'))
-	{
-		message.pop_back();
-	}
-	
-	//Quit command
-	if (message == "/quit")
-	{
-		break;
-	}
-	
-	 //User command
-	 if(message == "/users")
-	 {
-		 string userList = getUserList();
-		 sendMessage(clientSocket, userList);
-		 
-		 continue;
-	 }
-	 
-	 string formattedMessage = username + ": " + message + "\n";
-	 
-	 cout << formattedMessage;
-	 broadcastMessage(formattedMessage, clientSocket);
- 
-	//Remove client from shared client list
-	removeClient(clientSocket);
- 
-	//Release socket
-	close(clientSocket);
-	
-	cout << username << " disconnected" << endl;
-	
-	string leaveMessage = "\n[SERVER} " + username + " left the chat.\n"
-	broadcastMessage(leaveMessage);
+    char buffer[BUFFER_SIZE];
+
+    // Prompt client for username
+    string request = "Enter your username: ";
+    sendMessage(clientSocket, request);
+
+    int bytesReceived = recv(
+        clientSocket,
+        buffer,
+        BUFFER_SIZE,
+        0
+    );
+
+    if (bytesReceived <= 0)
+    {
+        close(clientSocket);
+        return;
+    }
+
+    string username(buffer, bytesReceived);
+
+    // Remove trailing newline characters
+    while (!username.empty() &&
+           (username.back() == '\n' || username.back() == '\r'))
+    {
+        username.pop_back();
+    }
+
+
+    // Add client to shared client list
+    {
+        lock_guard<mutex> lock(clientsMutex);
+
+        Client newClient;
+
+        newClient.socket = clientSocket;
+        newClient.username = username;
+
+        clients.push_back(newClient);
+    }
+
+
+    cout << username << " connected." << endl;
+
+    string joinMessage =
+        "\n[SERVER] " + username + " joined the chat.\n";
+
+    broadcastMessage(joinMessage, clientSocket);
+
+
+    string welcomeMessage =
+        "\nWelcome, " + username + "!\n"
+        "Commands:\n"
+        "/users - show connected users\n"
+        "/quit - disconnect\n\n";
+
+    sendMessage(clientSocket, welcomeMessage);
+
+
+    // Receive messages from this client
+    while (true)
+    {
+        bytesReceived = recv(
+            clientSocket,
+            buffer,
+            BUFFER_SIZE,
+            0
+        );
+
+        if (bytesReceived <= 0)
+        {
+            break;
+        }
+
+        string message(buffer, bytesReceived);
+
+
+        // Remove trailing newline characters
+        while (!message.empty() &&
+               (message.back() == '\n' || message.back() == '\r'))
+        {
+            message.pop_back();
+        }
+
+
+        // Quit command
+        if (message == "/quit")
+        {
+            break;
+        }
+
+
+        // Users command
+        if (message == "/users")
+        {
+            string userList = getUserList();
+
+            sendMessage(
+                clientSocket,
+                userList
+            );
+
+            continue;
+        }
+
+
+        string formattedMessage =
+            username + ": " + message + "\n";
+
+        cout << formattedMessage;
+
+        broadcastMessage(
+            formattedMessage,
+            clientSocket
+        );
+    }
+
+
+    // Remove client from shared client list
+    removeClient(clientSocket);
+
+
+    // Release socket
+    close(clientSocket);
+
+
+    cout << username << " disconnected." << endl;
+
+
+    string leaveMessage =
+        "\n[SERVER] " + username + " left the chat.\n";
+
+    broadcastMessage(leaveMessage);
 }
+
 
 int main()
 {
-	//Creating TCP socket
-	int serverSocket = socket(AF_INET, SOCKET_STREAM, 0);
-	
-	if(serverSocket == -1)
-	{
-		cerr << "Could not create a socket" << endl;
-		
-		return 1;
-	}
-	
-	//Allow socket address reuse
-	int option = 1;
-	
-	setsockop(serverSocket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option);
-	
-	sockaddr_in serverAddress{};
-	serverAddress.sin_family = AF_INET;
-	
-	serverAddress.sin_port = htons(PORT);
-	
-	serverAddress.sin_addr.s_addr = INADDR_ANY;
-	
-	//bind socket to port
-	if(blind(serverSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)
-		) == -1)
-	{
-		cerr << "Could not bind socket." << endl;
-		
-		close(serverSocket);
-		
-		return 1;
-	}
-	
-	//Start listening
-	if(listen(serverSocket, SOMAXCONN) == -1)
-	{
-		cerr << "Could not listen on the socket." << endl;
-		
-		close(serverSocket);
-		
-		return 1;
-	}
-	
-	cout << "Chat server started." << endl;
-	cout << "Listening on port." << endl;
-	
-	//Continously accepts new clients
-	while(true)
-	{
-		sockaddr_in clientAddress{};
-		
-		socklen_t clientSize = sizeof(clientAddress);
-		
-		int clientSocket = accept(serverSocket, (sockaddr*)&clientAddress, &clientSize);
-		
-		if(clientSocket == -1)
-		{
-			cerr << "Failed to accept client." << endl;
-			continue;
-		}
-		
-		char clientIP[INET_ADDRSTRLEN];
-		
-		inet_ntop(AF_INET, &clientAddress.sin_addr, clientIP, INET_ADDRSTRLEN);
-		
-		cout << "New connection from " << clientIP << endl;
-		
-		//Create one thread for this client
-		thread clientThread(handleClient, clientSocket);
-		clientThread.detach();
-	}
-	close(serverSocket);
-	return 0;
-}
+    // Creating TCP socket
+    int serverSocket = socket(
+        AF_INET,
+        SOCK_STREAM,
+        0
+    );
 
-	
-	
-	
-	
-	
-	
+
+    if (serverSocket == -1)
+    {
+        cerr << "Could not create a socket." << endl;
+
+        return 1;
+    }
+
+
+    // Allow socket address reuse
+    int option = 1;
+
+    if (setsockopt(
+            serverSocket,
+            SOL_SOCKET,
+            SO_REUSEADDR,
+            &option,
+            sizeof(option)
+        ) == -1)
+    {
+        cerr << "Could not set socket options." << endl;
+
+        close(serverSocket);
+
+        return 1;
+    }
+
+
+    sockaddr_in serverAddress{};
+
+    serverAddress.sin_family = AF_INET;
+
+    serverAddress.sin_port = htons(PORT);
+
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+
+    // Bind socket to port
+    if (bind(
+            serverSocket,
+            (sockaddr*)&serverAddress,
+            sizeof(serverAddress)
+        ) == -1)
+    {
+        cerr << "Could not bind socket." << endl;
+
+        close(serverSocket);
+
+        return 1;
+    }
+
+
+    // Start listening
+    if (listen(serverSocket, SOMAXCONN) == -1)
+    {
+        cerr << "Could not listen on the socket." << endl;
+
+        close(serverSocket);
+
+        return 1;
+    }
+
+
+    cout << "Chat server started." << endl;
+
+    cout << "Listening on port "
+         << PORT
+         << "."
+         << endl;
+
+
+    // Continuously accept new clients
+    while (true)
+    {
+        sockaddr_in clientAddress{};
+
+        socklen_t clientSize =
+            sizeof(clientAddress);
+
+
+        int clientSocket = accept(
+            serverSocket,
+            (sockaddr*)&clientAddress,
+            &clientSize
+        );
+
+
+        if (clientSocket == -1)
+        {
+            cerr << "Failed to accept client." << endl;
+
+            continue;
+        }
+
+
+        char clientIP[INET_ADDRSTRLEN];
+
+
+        inet_ntop(
+            AF_INET,
+            &clientAddress.sin_addr,
+            clientIP,
+            INET_ADDRSTRLEN
+        );
+
+
+        cout << "New connection from "
+             << clientIP
+             << endl;
+
+
+        // Create one thread for this client
+        thread clientThread(
+            handleClient,
+            clientSocket
+        );
+
+        clientThread.detach();
+    }
+
+
+    close(serverSocket);
+
+    return 0;
+}
